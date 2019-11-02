5bec91000503377ef51d5d382441b613ef95f972
Uongjo <Uongjo@utexas.edu>
2019-11-02 14:44:28 -0500

    Add files

---
+++ front-end/src/pages/Splash.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Splash extends Component {
+    
+  render() { 
+    return (
+      <h1>I am the splash page</h1>
+    );
+  }
+}
+ 
+export default Splash;
