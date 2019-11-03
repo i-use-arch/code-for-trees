2de4e4d73f837cddf034bda0df22365edc2d2fcf
Sam Wang <samuel.joshua.wang@gmail.com>
2019-11-03 10:42:12 -0600

    Finished Router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,29 @@ import Splash from "./pages/Splash";
 import Problem from "./pages/Problem";
 import Problems from "./pages/Problems";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./styles/main.scss";
 
 class App extends Component {
   state = {};
   render() {
     return (
+      // <Router>
+      //   <div className="App">
+      //     <nav>
+      //       <Problems />
+      //     </nav>
+
+      //     <Switch>
+      //       <Route path="/Two-Sum">
+      //         <Problem />
+      //       </Route>
+      //     </Switch>
+      //   </div>
+      // </Router>
       <div className="App">
-        <Splash />
-        {<Problem />}
-        {/* <Problems /> */}
+        <Problems />
       </div>
     );
   }
--- front-end/src/pages/Problem.js
+++ front-end/src/pages/Problem.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import AceEditor from 'react-ace';
+import React, { Component } from "react";
+import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/mode-javascript";
@@ -10,33 +10,45 @@ import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/theme-xcode";
 
 class Problem extends Component {
-  state = {}
+  state = {};
 
   constructor(props) {
     super(props);
     this.state = {
-      code: "",
-    }
+      code: ""
+    };
   }
 
-  onChange = (code) => {
+  onChange = code => {
     console.log(code);
-  }
+  };
 
-  render() { 
+  render() {
     return (
       <div id="problem">
         <div className="left-panel">
           <h1 className="problem-title">TWO SUM</h1>
-          <hr/>
-          <p>Given an array of integers, return indices of the two numbers such that they add up to a specific target.</p>
-          <p>You may assume that each input would have exactly one solution, and you may not use the same element twice.</p>
+          <hr />
+          <p>
+            Given an array of integers, return indices of the two numbers such
+            that they add up to a specific target.
+          </p>
+          <p>
+            You may assume that each input would have exactly one solution, and
+            you may not use the same element twice.
+          </p>
           <h2>Example</h2>
           <div className="example">
-            <p>Given <code>nums = [2, 7, 11, 15], target = 9</code>,</p>
+            <p>
+              Given <code>nums = [2, 7, 11, 15], target = 9</code>,
+            </p>
 
-            <p>Because <code>nums[0] + nums[1] = 2 + 7 = 9</code>,</p>
-            <p><code>return [0, 1].</code></p>
+            <p>
+              Because <code>nums[0] + nums[1] = 2 + 7 = 9</code>,
+            </p>
+            <p>
+              <code>return [0, 1].</code>
+            </p>
           </div>
         </div>
         <div className="right-panel">
@@ -56,11 +68,13 @@ class Problem extends Component {
               onChange={this.onChange}
             />
           </div>
-          <button onClick={() => console.log("HERE", this.state.code)}>SUBMIT</button>
+          <button onClick={() => console.log("HERE", this.state.code)}>
+            SUBMIT
+          </button>
         </div>
       </div>
     );
   }
 }
- 
+
 export default Problem;
